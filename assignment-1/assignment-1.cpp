17d8399e2c4983f7e35946685fbcfe1b2f099489
Geneva Porter <geneva.porter@gmail.com>
2020-02-06 18:23:00 -0800

    running a simple c++ program

---
+++ assignment-1/assignment-1.cpp
@@ -0,0 +1,18 @@
+/*
+ * Homework 0:
+ * 1. Use strictly serial computations (matrix-vactor, vector-matrix)
+ * 2. Measure execution times of multiplication
+ * 3. Make FAST and ELEGANT!
+ * 4. Use <gprof> profiler and check for memory leaks with <valgrind>
+ *
+ * valgrind --tool=memcheck --leak-check=yes ./your_binary
+ *
+ * Check out compiler explorer at godbolt.org
+ */
+int main() {
+
+
+
+
+	return 0;
+}
