99d67127fecfa1600bb3d80917f191d8680f4dae
Geneva Porter <geneva.porter@gmail.com>
2020-02-20 16:05:01 -0800

    starting assignment 1

--- assignment-1/assignment-1.cpp
+++ assignment-1/assignment-1.cpp
@@ -9,10 +9,41 @@
  *
  * Check out compiler explorer at godbolt.org
  */
-int main() {
 
 
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+//using namespace std;
+
+int gettimeofday(struct timeval *tv, struct timezone *tz);
+int settimeofday(const struct timeval *tv, const struct timezone *tz);
+
+struct timeval {
+	time_t tv_sec;
+	suseconds_t tv_usec;
+};
+
+struct timezone {
+	int tz_minutesweat;
+	int tz_dsttime;
+};
+
+
+int main(int argc, char **argv) {
+	struct timeval1 start, end;
+	gettimeofday(&start, NULL);
+
+	
+
+	for (int i = 0; i < 100; i++)
+	{
+		
+	}
+
+	gettimeofday(&end, NULL);
+	printf("ELapsed: %fs\n", ((ens.tv_sec - start.tv_sec)*100000 + end.tv_usec - start.tv_usec)/1000000.0);
 
 	return 0;
 }
