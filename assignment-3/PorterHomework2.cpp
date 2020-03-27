8b3c13aa9efa5dba59756ee034312eb2f02f7513
Geneva Porter <geneva.porter@gmail.com>
2020-03-26 22:24:03 -0700

    completed code for HW2

--- assignment-3/PorterHomework2.cpp
+++ assignment-3/PorterHomework2.cpp
@@ -5,7 +5,6 @@
  * This assignment approximates pi using the random point ratio method and utilizing OpenMP.
  */
 
-#include <sys/time.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <fstream>
@@ -13,6 +12,7 @@
 #include <string>
 #include <iomanip>
 #include <math.h>
+#include <omp.h>
 
 using namespace std;
 
@@ -34,29 +34,20 @@ double random2 () {
 
 
 
-int main(/*int argc, char **argv*/) {
+int main(int argc, char **argv) {
+
+    // Deciding number of threads
+    int thread_count = strtol(argv[1], NULL, 10);
 
     // Deciding number of iterations
-    int N = 100000000;
+    int N = 1000000;
     double distance, squared, ratio, interior = 0;
     int mod_step = N/10;
 
-    // Setting up time measurement variables
-    int gettimeofday(struct timeval *tv, struct timezone *tz);
-    int settimeofday(const struct timeval *tv, const struct timezone *tz);
-    double time_now, time_check;
-    struct timeval start, end;
-    gettimeofday(&start, NULL);
-
-    // Opening iteration table .txt file
-    //ofstream solution("solution-table.txt");
-    //solution << "Iterations Approximation";
-    //solution << '\n';
+    // Setting up table of values to print
     cout << setw(15) << left << "Iterations" 
 	 << setw(15) << left << "Approximation"  
-	 << setw(15) << left << "Thread ID" << '\n'; 
-	 
-
+	 << setw(15) << left << "Thread # (printing)" << '\n';
 
     // Allocating Memory
     double *d, *x, *y, *p;
@@ -65,55 +56,46 @@ int main(/*int argc, char **argv*/) {
     y = new double[N];
     p = new double[N];
 
-    
-    // Loop to find ratio
-    #pragma omp parallel for
-    for(int i = 0; i <= N; i++) {
-	x[i] = random2();
-	y[i] = random2();
-    }
+    #pragma omp 
+    {
 
-    #pragma omp parallel for
-    for(int i = 0; i <= N; i++) {
-	d[i] = pow(x[i]*x[i] + y[i]*y[i], 0.5);
-    }
+        #pragma omp parallel for schedule(static) num_threads(thread_count) 
+        for(int i = 0; i <= N; i++) {
+	    x[i] = random2();
+	    y[i] = random2();
+        }
 
-    //#pragma omp parallel for
-    for(int i = 0; i <= N; i++) {
-	if (d[i] <= 1) {
-	    interior ++;
-	}
-	p[i] = 4 * interior / (i+1);
-    }
+        #pragma omp parallel for schedule(static) num_threads(thread_count) 
+        for(int i = 0; i <= N; i++) {
+	    d[i] = pow(x[i]*x[i] + y[i]*y[i], 0.5);
+        }
 
-    gettimeofday(&end, NULL);
-    time_now = ((end.tv_sec - start.tv_sec)*1000000 + end.tv_usec - start.tv_usec)/2;
-    time_check = time_now / 60000000;
-    if (time_check >= 30) {
-	cout << "Run time has exceeded 30 minutes. Ending program now.";
-	return 0;
-    }
+        for(int i = 0; i <= N; i++) {
+	    if (d[i] <= 1) {
+	        interior ++;
+	    }
+	    p[i] = 4 * interior / (i+1);
+        }
 
-    for(int i = 1; i <= N; i++) {
+        #pragma omp parallel for num_threads(thread_count) 
+        for(int i = 1; i <= N; i++) {
+	    // Printing Pi approximations at regular intervals for comparison
+            if (i == 10     || i == 100     || i == 1000 || i == 10000 || 
+	        i == 100000 || i == 1000000 || i % mod_step == 0) {
+	        cout << setw(15) << left << i 
+	 	     << setw(15) << left << setprecision(10) << p[i-1] 
+		     << setw(15) << left << omp_get_thread_num() << '\n'; 
+	    }
 
-	// Printing Pi approximations at regular intervals for comparison
-        if (i == 10     || i == 100     || i == 1000 || i == 10000 || 
-	    i == 100000 || i == 1000000 || i % mod_step == 0) {
-            //solution << i << '\t' << ratio << '\n';
-	    cout << setw(15) << left << i 
-	 	 << setw(15) << left << setprecision(10) << p[i-1] 
-		 << setw(15) << left << "thread_id" << '\n'; 
-	}
-
+        }
     }
 
 
     // Deallocating memory
-    delete [] p;
+    delete [] d;
     delete [] x;
     delete [] y;
-
-    //solution.close();
+    delete [] p;
 
     return 0;
 }
